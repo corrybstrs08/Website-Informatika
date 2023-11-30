ff636def930ef8c80d96e49d79b5e6ad17fd4cd8
11S21047 <corrysitorus03@gmail.com>
2023-11-30 08:57:05 +0700

    modifikasi pada modul dosen

--- resources/views/staffDosen.blade.php
+++ resources/views/staffDosen.blade.php
@@ -1,22 +1,22 @@
 @extends('master')
 
 @section('section')
-<h1 class="text-light ms-5"><b>Dosen</b></h1>
+<h1 class="serv text-body"><b>Dosen</b></h1>
 <section>
     <div class="serv text-body">
         <ul>
             @foreach ($dosen as $item)
-
-            <div class="card opacity-50" style="width: 18rem; height: 10rem; border: radius 30%;">
-                <div>
-                    <div class="d-flex justify-content-center mt-3">
-                        <img class="gbrDosen " src="{{ asset('asset/img/Dosen/' . $item->gambar) }}" alt="{{ $item->gambar }}">
+            <a href="/detailDosen/{{ $item->id }}" class="card-link">
+                <div class="card opacity-50" style="width: 18rem; height: 10rem; border: radius 30%;">
+                    <div>
+                        <div class="d-flex justify-content-center mt-3">
+                            <img class="gbrDosen" src="{{ asset('asset/img/Dosen/' . $item->gambar) }}" alt="{{ $item->gambar }}">
+                        </div>
+                        <br>
+                        <p class="text-center dosen-name"><strong>{{ $item->nama }}</strong></p>
                     </div>
-                    <br>
-                    <p class="text-center ">{{ $item->nama }}</p>
                 </div>
-            </div>
-
+            </a>
             @endforeach
         </ul>
     </div>
