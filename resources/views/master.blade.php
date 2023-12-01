5efc412001bcbfa0460786c8013ae2c69dec3c90
ifs21034-itdel <ifs21034@students.del.ac.id>
2023-12-01 11:52:14 +0700

    modifikasi pencarian

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('asset/css/bootstrap-5.2.3-dist/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('asset/css/bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
@@ -283,8 +285,10 @@
 
               <div class="row  d-flex justify-content-end">
                 <div class="col col-6 " style="width: 300px;">
-                  {{ csrf_field() }}
-                  <input type="text" class="form-control" placeholder="Pencarian.." style="background-color: aliceblue;" name="cari" onclick="clearInput(this)">
+                  <form action="/cariProses" method="post" enctype="multipart/form-data">
+                    {{ csrf_field() }}
+                    <input type="text" class="form-control" placeholder="Pencarian.." style="background-color: aliceblue;" name="cari" onclick="clearInput(this)">
+                  </form>
                 </div>
               </div>
             </div>
