83eaca9bb6a1a71bc7ea61eae8391c4303bc8c3b
11S21047 <corrysitorus03@gmail.com>
2023-12-01 13:53:48 +0700

    modif pada tampilan  dosen

--- resources/views/staffDosen.blade.php
+++ resources/views/staffDosen.blade.php
@@ -7,7 +7,7 @@
             <ul class="list-unstyled d-flex flex-wrap justify-content-around">
                 @foreach ($dosen as $item)
                     <a href="/detailDosen/{{ $item->id }}" class="card-link">
-                        <div class="card m-2" style="width: 18rem;">
+                        <div class="card m-2" style="width: 18rem; height: 10rem;">
                             <div class="text-center mt-3">
                                 <img class="gbrDosen img-fluid" src="{{ asset('asset/img/Dosen/' . $item->gambar) }}" alt="{{ $item->gambar }}">
                                 <p class="text-center dosen-name mt-3"><strong>{{ $item->nama }}</strong></p>
